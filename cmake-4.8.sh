b2b08bb73e682e5d79b43c1b979d0eda86fe567a
Daniel Duque <daniel.duque@gmail.com>
2016-07-29 13:23:41 +0200

    Slight tidy-up

--- cmake-4.8.sh
+++ cmake-4.8.sh
@@ -1,3 +1,5 @@
-cmake -DCGAL_DIR=/home/daniel/CGAL-4.8.1 -DCMAKE_BUILD_TYPE=Release
+CGAL_DIR=$HOME/CGAL-4.8.1
+#cmake -DCGAL_DIR=/home/daniel/CGAL-4.8.1 -DCMAKE_BUILD_TYPE=Release
+cmake -DCMAKE_BUILD_TYPE=Release
 #Debug
 #Release
