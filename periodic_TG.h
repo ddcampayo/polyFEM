a3018ab3530e5eb6dd7924d112e11e0192153338
Daniel Duque <daniel.duque@gmail.com>
2016-09-21 12:48:10 -0700

    periodic different

---
+++ periodic_TG.h
@@ -0,0 +1,18 @@
+//#include"main.h"
+
+const FT LL=1; // length of original domain
+
+FT per_dist(const FT& x1, const FT& x2);
+Point per_point(const FT& x1,const  FT& x2);
+Point per_point(const Point& p);
+Vector_2 per_vect(const Point& P1, const Point& P2);
+FT per_dist2(const Point& P1, const Point& P2);
+FT per_area(const Point& P0, const Point& P1, const Point& P2);
+
+
+
+
+
+
+
+
