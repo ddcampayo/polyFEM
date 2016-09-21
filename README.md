e8145f97f717028f34d8f39ddd9249c258adb198
Daniel Duque <daniel.duque@gmail.com>
2016-09-21 13:42:43 -0700

    Improved diff between cases

--- README.md
+++ README.md
@@ -5,8 +5,19 @@ A free and open source pFEM simulation tool, based on the CGAL libs.
 This branch covers the procedure of "assignment": transferring information
 from a set of moving particles onto a fixed mesh, and back
 
-For different simulations, simply copy any of the "main" files and name it
-"main.cpp". Then, run "source cmake-4.8.sh", and "make"
+# Case select
+For different simulations, simply copy any of the main_*.cpp files onto
+main.cpp. Same with periodic_*.h onto periodic.h
+
+Then, run "source cmake-4.8.sh", and "make"
+
+Sample simu_*.cfg input files are provided, copy them onto simu.cfg in
+order to run.
+
+Suggested command to run:
+
+main >& log&
+
 
 # Features
 Currently provided:
@@ -17,3 +28,5 @@ The rotating Zalesak disk test (main_Zalesak.cpp)
 
 CGAL 4.8
 eigen3 libs
+
+
---
+++ tools/movie_TG.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import pylab as pl
+
+def grid(x, y, z , resX=100, resY=100):
+    "Convert 3 column data to matplotlib grid"
+    xi = pl.linspace(min(x), max(x), resX)
+    yi = pl.linspace(min(y), max(y), resY)
+    Z = pl.griddata(x, y, z, xi, yi , interp='linear')
+    X, Y = pl.meshgrid(xi, yi )
+    return X, Y, Z
+
+pl.figure(figsize=(8,8))
+
+skip=2
+#path='timings_full/'
+path='./'
+
+for n in range(0,400+skip,skip):
+    pl.clf()
+ #   dtm=pl.loadtxt(path+str(n)+'/particles.dat')
+    dtm=pl.loadtxt(path+str(n)+'/mesh.dat')
+    xm=dtm[:,0]; ym=dtm[:,1];  pm=dtm[:,5];  vxm=dtm[:,8]; vym=dtm[:,9]; alm=dtm[:,4]
+#    p1 = dtm[:,5] - pl.mean(dtm[:,5] )
+#    dt2=pl.loadtxt(path+'fem1/'+str(n)+'/particles.dat')
+#    dt2=pl.loadtxt(path+str(n)+'/mesh.dat')
+    X, Y, Z = grid(xm, ym, alm)
+
+    pl.contourf( X,Y,Z,[-1,0.45,0.55,2])
+
+    pl.xlim([-0.7,0.7])
+    pl.ylim([-0.7,0.7])
+#    pl.colorbar(ticks=[0.45,0.55])
+    pl.savefig('snap'+str(n/skip))
