bc9363e585a532caca084e8f5d0f7cf65dab270f
ddcampayo <daniel.duque@gmail.com>
2017-11-16 18:24:20 +0100

    refining CH method

--- tools/make_movie.sh
+++ tools/make_movie.sh
@@ -1,1 +1,2 @@
-avconv -r 10 -i parts%1d.png -c:v libx264 -pix_fmt yuv420p   movie.mp4
+##  ffmpeg -r 1 -i parts%5d.png -c:v libx264 -pix_fmt yuv420p -r 2 movie.mp4
+ffmpeg -r 1 -pattern_type glob -i "*.png"  -c:v libx264 -pix_fmt yuv420p -r 4 movie.mp4
--- tools/parts_movie.py
+++ tools/parts_movie.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-LL=128.0
+LL=32.0
 
 import pylab as pl
 
 pl.figure(figsize=(8,8))
 
-skip=10
+skip=5
 begin=00
 end=10000
 limits=1
@@ -20,13 +20,15 @@ for n in range(begin,end+skip,skip):
     xm=dtm[:,0]; ym=dtm[:,1];  pm=dtm[:,5];  vxm=dtm[:,8]; vym=dtm[:,9]; alm=dtm[:,4]
 
 #    pl.scatter( xm , ym , c=alm, s=80, vmin= -limits , vmax= limits)
-    pl.scatter( xm , ym , c=alm, s=10*LL/64.0 , linewidths=0 )
+    pl.scatter( xm , ym , c=alm, s=640/LL , cmap= pl.cm.bwr , linewidths=0 )
 
     pl.xlim([ -LL/2 , LL/2 ])
     pl.ylim([ -LL/2 , LL/2 ])
     pl.colorbar()
 #    pl.colorbar(ticks=[0.45,0.55])
-#    pl.savefig('parts'+str(n/skip))
-    pl.savefig('parts'+str(n))
+#    pl.savefig('parts'+str(n/skip)  )
+
+    pl.savefig('parts'+str(n).zfill(5) )
+
 #  pl.scatter( xm , ym , c=alm, s=20*LL/64.0 , linewidths=0 , cmap= pl.get_cmap(name="binary"))
 
